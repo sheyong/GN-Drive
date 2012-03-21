61d2ff13c9e7f37e18fce27ec9dc298b65831ae4
passerbycrk <passerbycrk@gmail.com>
2012-03-21 12:03:08 +0800

    添加NSString和UIDevice扩展方法

---
+++ iPhoneClient/TwinDriveSystem/TwinDriveSystem/Class/General/TDSGlobal.m
@@ -0,0 +1,10 @@
+//
+//  TDSGlobal.h
+//  TwinDriveSystem
+//
+//  Created by zhongsheng on 12-2-29.
+//  Copyright (c) 2012年 renren. All rights reserved.
+//
+
+#import "TDSGlobal.h"
+// 为了在h文件 添加import和define有提示
---
+++ iPhoneClient/TwinDriveSystem/TwinDriveSystem/Class/Tool/NSString+NSStringExt.h
@@ -0,0 +1,19 @@
+//
+//  NSString+NSStringEx.h
+//  TwinDriveSystem
+//
+//  Created by 自 己 on 12-3-21.
+//  Copyright (c) 2012年 renren. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Addition)
+/**
+ * 将字符串MD5加密.
+ * 
+ * @return 加密后的字符串.
+ */
+- (NSString*) MD5String;
+
+@end
